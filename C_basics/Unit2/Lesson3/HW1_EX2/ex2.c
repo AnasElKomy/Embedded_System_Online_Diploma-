0168050b22c94a046de3ef148e01d2a710324dd1
AnasElKomy <anaslkomy28@gmail.com>
2021-10-18 17:50:49 +0200

    Assigns

---
+++ C_basics/Unit2/Lesson3/HW1_EX2/ex2.c
@@ -0,0 +1,15 @@
+/*
+ * main.c
+ *
+ *  Created on: Oct18, 2021
+ *      Author: Anas
+ */
+#include <stdio.h>
+int main ()
+{
+  int Number ;
+  printf("Enter an integer : ");
+  scanf("%d",&Number);
+  printf("You entered : %d ",Number);
+  return 0 ;
+}
