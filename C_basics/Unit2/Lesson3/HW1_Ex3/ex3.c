8e6b754f81f8766700b85f8d00f0c6060aaba2db
AnasElKomy <anaslkomy28@gmail.com>
2021-10-18 18:13:34 +0200

    Example3

---
+++ C_basics/Unit2/Lesson3/HW1_Ex3/ex3.c
@@ -0,0 +1,15 @@
+/*
+ * main.c
+ *
+ *  Created on: Oct18, 2021
+ *      Author: Anas
+ */
+#include <stdio.h>
+int main ()
+{
+  int x , y ;
+  printf("Enter two integers: ");
+  scanf("%d%d",&x,&y);
+  printf("Sum: %d",x+y);
+  return 0 ;
+}
