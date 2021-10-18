e19782adbf3fba89b3cbdb5e90a737dc42fc13e0
AnasElKomy <anaslkomy28@gmail.com>
2021-10-18 18:32:22 +0200

    Find ASCII Value of a Character

---
+++ C_basics/Unit2/Lesson3/HW1_Ex5/ex5.c
@@ -0,0 +1,15 @@
+/*
+ * main.c
+ *
+ *  Created on: Oct18, 2021
+ *      Author: Anas
+ */
+#include <stdio.h>
+int main ()
+{
+  char character ;
+  printf("Enter a character: ");
+  scanf("%c",&character);
+  printf("ASCII value of %c = %d",character,character);
+  return 0 ;
+}
