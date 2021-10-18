a4c6a04cd91f4077b101e476eae5da32f7e2acb8
AnasElKomy <anaslkomy28@gmail.com>
2021-10-18 18:22:49 +0200

    Multiply two Floating Point Numbers

---
+++ C_basics/Unit2/Lesson3/HW1_EX4/ex4.c
@@ -0,0 +1,15 @@
+/*
+ * main.c
+ *
+ *  Created on: Oct18, 2021
+ *      Author: Anas
+ */
+#include <stdio.h>
+int main ()
+{
+  float x ,y;
+  printf("Enter two Numbers: ");
+  scanf("%f%f",&x,&y);
+  printf("Product: %f",x*y);
+  return 0 ;
+}
