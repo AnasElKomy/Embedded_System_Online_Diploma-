c6a58528e43917f35661b5f176a341093bcce867
AnasElKomy <anaslkomy28@gmail.com>
2021-11-07 16:46:06 +0200

    Calculate power of number using Recusion

---
+++ C_basics/Unit2/Lesson5/Ex4/ex4.c
@@ -0,0 +1,25 @@
+/*
+ * main.c
+ *
+ *  Created on: Nov 7, 2021
+ *      Author: Anas
+ */
+ #include <stdio.h>
+ int power (int base,int exp);
+ int main()
+ {
+   int base , exp ;
+   printf("Enter base number: ");
+   scanf("%d",&base);
+   printf("Enter power number(positive integer): ");
+   scanf("%d",&exp);
+   printf("%d^%d = %d ",base,exp,power(base,exp));
+   return 0;
+ }
+ int power (int base,int exp)
+ {
+   if (exp>=1)
+    return (base*power(base,exp-1));
+  else
+    return 1 ;
+ }
